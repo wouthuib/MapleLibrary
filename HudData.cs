aade3165f2b4e7c23ad4b5e9911b3b5a3b946454
wouthuib <wouthuib@gmail.com>
2014-12-27 22:44:14 +0100

    update 27 December 2014
    new HudData

--- HudData.cs
+++ HudData.cs
@@ -5,20 +5,20 @@ using System.Security.Permissions;
 namespace MapleLibrary
 {
     [Serializable]
-    public class ItemData : ISerializable
+    public class HudData : ISerializable
     {
-        public int ID { get; set; }
         public string action { get; set; }
+        public int value { get; set; }
         public string player_name { get; set; }
 
-        public ItemData() { }
+        public HudData() { }
 
-        protected ItemData(SerializationInfo info, StreamingContext context)
+        protected HudData(SerializationInfo info, StreamingContext context)
         {
             if (info == null)
                 throw new System.ArgumentNullException("info");
-            ID = (int)info.GetValue("ID", typeof(int));
             action = (string)info.GetValue("action", typeof(string));
+            value = (int)info.GetValue("value", typeof(int));
             player_name = (string)info.GetValue("player_name", typeof(string));
         }
 
@@ -28,8 +28,8 @@ namespace MapleLibrary
         {
             if (info == null)
                 throw new System.ArgumentNullException("info");
-            info.AddValue("ID", ID);
             info.AddValue("action", action);
+            info.AddValue("value", value);
             info.AddValue("player_name", player_name);
         }
     }
